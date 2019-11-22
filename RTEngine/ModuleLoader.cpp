57079dc477c58902678a9fd43174b8db8c92842c
Lorién <lorienportella@gmail.com>
2019-11-22 20:41:53 +0100

    scen name changing done

--- RTEngine/ModuleLoader.cpp
+++ RTEngine/ModuleLoader.cpp
@@ -543,7 +543,7 @@ bool ModuleLoader::ExportScene()
 	if (ret)
 	{
 		std::string path_buffer;
-		FileSystem::FormFullPath(path_buffer, App->scene->name.c_str(), ASSETS_SCENES,".westscene");
+		FileSystem::FormFullPath(path_buffer, App->scene->scene_name.c_str(), ASSETS_SCENES,".westscene");
 		json_serialize_to_file(file, path_buffer.c_str());
 	}
 	else
